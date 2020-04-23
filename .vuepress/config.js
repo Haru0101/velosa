d3ebcb1786a52dcd2f7734eba60ab81c3956514c
Haruki Okuda <sunny_smile_0v0@yahoo.co.jp>
2020-04-23 23:14:55 +0900

    5005f01

---
+++ .vuepress/config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    title: 'Enger.',
+    description: 'UIとインタラクティブ表現を追求して',
+    themeConfig: {
+        sidebar: {},
+        nav: [{
+                text: 'Home',
+                link: '/'
+            },
+            {
+                text: 'About',
+                link: '/about/'
+            },
+            {
+                text: 'Articles',
+                link: '/articles/'
+            }
+        ]
+    },
+    dest: 'docs/',
+    base: '/enger/',
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+---
+home: true
+heroImage: /img/img_hero.jpg
+heroText: Enger.
+footer: Copyright 2020 Haruki
+---
+
