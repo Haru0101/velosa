8d15b6003b4004f6302e35f191b8c32da7276a18
Haruki Okuda <sunny_smile_0v0@yahoo.co.jp>
2020-04-27 20:45:56 +0900

    ファビコンの設置

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,6 +1,12 @@
 module.exports = {
     title: 'velosa',
     description: 'UIとインタラクティブ表現を追求して',
+    head: [
+        ['link', {
+            rel: 'icon',
+            href: '/img/favicon.png'
+        }]
+    ],
     themeConfig: {
         logo: '/img/img_logo.png',
         sidebar: {},
