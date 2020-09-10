782bc0e9587cfa3c920dcf9813a0e185d583b56b
Haruki Okuda <sunny_smile_0v0@yahoo.co.jp>
2020-09-10 20:12:28 +0900

    PHPを非表示にしてWordPressをトップに表示
    Signed-off-by: Haruki Okuda <sunny_smile_0v0@yahoo.co.jp>

--- src/.vuepress/theme/components/Articles.vue
+++ src/.vuepress/theme/components/Articles.vue
@@ -24,10 +24,16 @@
                 <Categories :pages="this.$site.pages" prefix="/articles/vuejs" />
             </div>
         </div>
-        <div class="category__list-wrap">
+        <!-- <div class="category__list-wrap">
             <div class="category__list-inner">
                 <h2>PHP</h2>
                 <Categories :pages="this.$site.pages" prefix="/articles/php" />
+            </div>
+        </div> -->
+        <div class="category__list-wrap">
+            <div class="category__list-inner">
+                <h2>WordPress</h2>
+                <Categories :pages="this.$site.pages" prefix="/articles/wordpress" />
             </div>
         </div>
         <div class="category__list-wrap">
@@ -41,13 +47,8 @@
                 <h2>Laravel</h2>
                 <Categories :pages="this.$site.pages" prefix="/articles/laravel" />
             </div>
-        </div>
-        <div class="category__list-wrap">
-            <div class="category__list-inner">
-                <h2>WordPress</h2>
-                <Categories :pages="this.$site.pages" prefix="/articles/wordpress" />
-            </div>
         </div> -->
+        
         <div class="category__list-wrap">
             <div class="category__list-inner">
                 <h2>Others</h2>
